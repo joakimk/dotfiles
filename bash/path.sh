0d2bdab10c3ba5709a91b2ddcd307647b2f4274b
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-01-07 13:29:49 +0100

    Fixed paths to install the mysql gem.

--- bash/path.sh
+++ bash/path.sh
@@ -7,6 +7,8 @@ export PATH=/Users/jocke/.scripts:/usr/local/bin:$PATH
 # Python
 export PATH=/usr/local/Cellar/python/2.6.5/bin:$PATH
 
+# Mysql
+export PATH=/usr/local/mysql/bin:$PATH
+
 # RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
-
