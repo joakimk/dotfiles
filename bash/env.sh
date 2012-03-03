87281955eb6a8164d26a1fc3b5335c9b5750ef44
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-03-03 11:24:14 +0100

    Changing editor to terminal vim for commits.

--- bash/env.sh
+++ bash/env.sh
@@ -1,9 +1,2 @@
-if test -n "$SSH_CONNECTION"; then
-  # Over SSH.
-  export EDITOR='vim'
-  export GIT_EDITOR='vim'
-else
-  # Not over SSH.
-  export EDITOR='mvim -f'
-  export GIT_EDITOR='mvim -f'
-fi
+export EDITOR='vim'
+export GIT_EDITOR='vim'
