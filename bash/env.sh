54aa19bf3d295339f555d6b49c6acf418b9a9e9f
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2018-02-14 10:58:11 +0100

    Enable shell history in iex

--- bash/env.sh
+++ bash/env.sh
@@ -1,6 +1,9 @@
 export EDITOR='vim'
 export GIT_EDITOR='vim'
 
+# Enable shell history in iex
+export ERL_AFLAGS="-kernel shell_history enabled"
+
 # Allow ctrl+s without locking the session when sshing
 bind -r '\C-s'
 stty -ixon
