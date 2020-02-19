7e2378e4f7aa2fe7243089ecaff2ac096debccfe
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2020-02-19 10:30:29 +0100

    Add more pairing aliases

--- bash/lib/gitpair.sh
+++ bash/lib/gitpair.sh
@@ -14,5 +14,7 @@ alias pairt="git config user.pair 'JK+TS' && git config user.name 'Joakim Kolsj�
 alias pairv="git config user.pair 'JK+VA' && git config user.name 'Joakim Kolsjö and Victor Arias' && git config user.email 'all+jocke+victor@barsoom.se'; pair"
 alias pairf="git config user.pair 'FH+JK' && git config user.name 'Joakim Kolsjö and Fabian Hoffmann' && git config user.email 'all+fabian+jocke@barsoom.se'; pair"
 alias pairl="git config user.pair 'JK+LF' && git config user.name 'Joakim Kolsjö and Lennart Fridén' && git config user.email 'all+jocke+lennart@barsoom.se'; pair"
+alias pairp="git config user.pair 'JK+PW' && git config user.name 'Joakim Kolsjö and Peter Wall' && git config user.email 'all+jocke+peter@barsoom.se'; pair"
 alias pairh="git config user.pair 'HN+JK' && git config user.name 'Henrik Nyh and Joakim Kolsjö' && git config user.email 'all+henrik+jocke@barsoom.se'; pair"
+alias pairht="git config user.pair 'HN+JK+TS' && git config user.name 'Henrik Nyh and Joakim Kolsjö and Tomas Skogberg' && git config user.email 'all+henrik+jocke+tomas@barsoom.se'; pair"
 alias pairmob="git config user.pair 'MOB' && git config user.name 'Mob' && git config user.email 'devs@auctionet.com'; pair"
